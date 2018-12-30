4dea021f3f00035205530aa23d04bcc3f6195306
Jacky Han <kisserleon@gmail.com>
2018-12-30 15:19:38 +0000

    move Beer into Guinness class

--- Annotation101/src/main/java/com/jacky/java101/annotation/built_in_annotation/Guinness.java
+++ Annotation101/src/main/java/com/jacky/java101/annotation/built_in_annotation/Guinness.java
@@ -1,5 +1,19 @@
 package com.jacky.java101.annotation.built_in_annotation;
 
+class Beer {
+  /**
+   * @deprecated use of name
+   * is discouraged, use
+   * getName instead.
+   */
+  @Deprecated
+  public String name() { return "Beer"; }
+  public String getName() {
+    return "Beer";
+  }
+}
+
+
 public class Guinness extends Beer {
   @SuppressWarnings("deprecation")
   @Override
